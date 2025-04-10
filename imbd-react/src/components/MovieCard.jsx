91f8a5d0a9571833aea28f6f5836e8b1e9a61f29
Josh <128964404+JoshErskine@users.noreply.github.com>
2025-04-10 08:57:29 +0100

    Movie card component

---
+++ imbd-react/src/components/MovieCard.jsx
@@ -0,0 +1,25 @@
+function MovieCard({ movie }) {
+  function handleClick() {
+    alert(`Added ${movie.Title} to favourites`);
+  }
+
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={movie.Poster} alt={movie.Title} />
+        <div className="movie-overlay">
+          <button className="favourite-btn" onClick={handleClick}>
+            ♥
+          </button>
+        </div>
+      </div>
+      <div className="movie-details">
+        <h3>{movie.Title}</h3>
+        <p>{movie.Year}</p>
+        <p>{movie.Genre}</p>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
