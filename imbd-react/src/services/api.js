61c3caac9fc4eb5c2f23f029708f4ffe6e8911a1
Josh <128964404+JoshErskine@users.noreply.github.com>
2025-04-12 11:04:15 +0100

    API set up with searchMovies and getPopularMovies

---
+++ imbd-react/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "679db35917a1b14ed89ad9d37f31fbe7";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = response.json();
+  return data.results;
+};
